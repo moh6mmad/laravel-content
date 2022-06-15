a99f7dc4bc0b92c3c3b6e519e9c73bf6b9da4267
Mohammad Hadizadeh <mohammad@lync.me>
2022-06-15 17:26:13 +0200

    Controller - Updated content controller for view files

--- src/App/Controllers/ContentController.php
+++ src/App/Controllers/ContentController.php
@@ -25,17 +25,7 @@ class ContentController extends Controller
         }
         
         $contents = $contents->orderBy('id', 'desc')->paginate(9);
-        return view('laravel-content::index', compact('contents'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     */
-    public function terms()
-    {
-        $page = Content::where('slug', 'terms')->firstOrFail();
-        return view('pages.index', $page);
+        return view('laravel-content::pages.index', compact('contents'));
     }
 
     /**
@@ -47,7 +37,7 @@ class ContentController extends Controller
     public function show($page_slug)
     {
         $page = Content::where('slug', $page_slug)->firstOrFail();
-        return view('pages.index', $page);
+        return view('laravel-content::pages.index', $page);
     }
     /**
      * Display the specified resource.
@@ -65,46 +55,7 @@ class ContentController extends Controller
 
         $page->related_contents = Content::inRandomOrder()->limit(2)->get();
 
-        return view('blog.show', $page);
-    }
-
-
-    
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function HelpItemWithCategory($category = '', $page_slug = '')
-    {
-        $page = Content::where('slug', $page_slug)->firstOrFail();
-        return view('help.show', $page);
-    }
-    
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function HelpItem($page_slug = '')
-    {
-        $page = Content::where('slug', $page_slug)->firstOrFail();
-        return view('help.show', $page);
-    }
-
-    
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function HelpIndex()
-    {
-        $helps = Content::where('content_group', 'help')->where('status', '1')->orderBy('category', 'asc')->get();
-        return view('help.index', compact('helps'));
+        return view('laravel-content::pages.show', $page);
     }
 
     /**
@@ -117,7 +68,7 @@ class ContentController extends Controller
     public function dynamic($urlkey = ''){
       
         $page = Content::where('slug', $urlkey)->firstOrFail();
-        return view('pages.index', $page);
+        return view('laravel-content::pages.index', $page);
     }
 
     
